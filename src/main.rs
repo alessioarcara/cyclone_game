648b60b1cb1b8da20f062e3e048d123d7c8f1fbd
alessioarcara <Spammer123@>
2023-06-01 18:54:27 +0200

    added menu

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,53 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("")]
+enum MenuOption {
+    CreateGame,
+    JoinGame,
+}
+
+struct State {
+    current_option: MenuOption
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            current_option: MenuOption::CreateGame,
+        }
+    }
+
+    fn draw(&self) {
+            match self.current_option {
+            MenuOption::CreateGame => {
+                draw_text("Create Game", 50.0, 50.0, 30.0, WHITE);
+                draw_text("Join Game", 50.0, 100.0, 30.0, GRAY);
+            }
+            MenuOption::JoinGame => {
+                draw_text("Create Game", 50.0, 50.0, 30.0, GRAY);
+                draw_text("Join Game", 50.0, 100.0, 30.0, WHITE);
+            }
+        }
+    }
+
+    fn update(&mut self) {
+        if is_key_pressed(KeyCode::Up) || is_key_pressed(KeyCode::Down) {
+            self.current_option = match self.current_option {
+                MenuOption::CreateGame => MenuOption::JoinGame,
+                MenuOption::JoinGame => MenuOption::CreateGame,
+            };
+        }
+    }
+}
+
+#[macroquad::main("Arrow Game")]
 async fn main() {
+    let mut state = State::new();
+
     loop {
         clear_background(BLACK);
 
-        draw_text("Create game", 20. , 20., 30., GRAY);
-        draw_text("Join game", 20., 40., 30., GRAY);
+        state.update();
+        state.draw();
 
         next_frame().await;
     } 
