a36e77baeeabd04a54fdaf0599abcf7e56091154
alessioarcara <Spammer123@>
2023-06-17 14:57:34 +0200

    removed specs crate and implemented state, update patterns

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,78 @@
-use core::panic;
+pub mod game;
+pub mod menu;
 
-use components::{Position, Renderable, Player, Wall};
-use entities::{create_floor, create_wall, create_player};
-use macroquad::texture::load_texture;
-use specs::{World, WorldExt, RunNow};
-use systems::{RenderingSystem, InputSystem};
+use menu::Menu;
 
-mod components;
-mod entities;
-mod systems;
-
-#[derive(Default)]
-pub struct DeltaTime(f64);
-
-pub fn register_resources(world: &mut World) {
-    world.insert(DeltaTime::default())
+pub trait GameObject {
+    fn input(&mut self);
+    fn update(&self);
+    fn draw(&self);
 }
 
-pub fn register_components(world: &mut World) {
-    world.register::<Position>();
-    world.register::<Renderable>();
-    world.register::<Player>();
-    world.register::<Wall>();
+pub struct GameManager {
+    state: Box<dyn GameObject> 
 }
 
-pub struct Game {
-    world: World,
-}
-
-impl Game {
-    pub fn new(world: World) -> Self {
+impl GameManager {
+    pub fn new() -> Self {
         Self {
-            world,
-        }
-    }
-
-    pub fn update(&self) {
-        {
-            let mut is = InputSystem {}; 
-            is.run_now(&self.world)
-        }
-    }
-
-    pub fn draw(&self) {
-        {
-            let mut rs =  RenderingSystem;
-            rs.run_now(&self.world)
+            state: Box::new(Menu::new()),
         }
     }
 }
 
-pub async fn initialize_level(world: &mut World) {
-    const MAP: &str = "
-    W W W W W W W W
-    W . . . . . . W
-    W . . . . . . W
-    W . . . . . . W 
-    W . . P . . . W
-    W . . . . . . W
-    W . . . . . . W
-    W . . . . . . W
-    W W W W W W W W
-    ";
-    load_map(world, MAP.to_string()).await;
-}
+impl GameObject for GameManager {
+    fn input(&mut self) {
+        self.state.input();
+    }
 
-pub async fn load_map(world: &mut World, map_string: String) {
-    let wall_texture = load_texture("../resources/wall.png").await.expect("Failed to load resource");
-    let floor_texture = load_texture("../resources/floor.png").await.expect("Failed to load resource");
-    let player_texture = load_texture("../resources/player.png").await.expect("Failed to load resource");
-    let rows: Vec<&str> = map_string.trim().split('\n').map(|x| x.trim()).collect();
+    fn update(&self) {
+        self.state.update(); 
+    }
 
-    for (y, &row) in rows.iter().enumerate() {
-        let columns: Vec<&str> = row.split(' ').collect();
-
-        for (x, &column) in columns.iter().enumerate() {
-            let x = x as f32;
-            let y = y as f32;
-            let position = Position { x, y, z: 0 };
-
-           match column {
-                "." => create_floor(world, position, floor_texture),
-                "W" => create_wall(world, position, wall_texture),
-                "P" => {
-                    create_floor(world, position, floor_texture);
-                    create_player(world, position, player_texture);
-                },
-                _ => panic!("unrecognized map item")
-            } 
-        }
+    fn draw(&self) {
+        self.state.draw();   
     }
 }
+
+//pub fn initialize_level() {
+//    const MAP: &str = "
+//    W W W W W W W W
+//    W . . . . . . W
+//    W . . . . . . W
+//    W . . . . . . W 
+//    W . . P . . . W
+//    W . . . . . . W
+//    W . . . . . . W
+//    W . . . . . . W
+//    W W W W W W W W
+//    ";
+//    load_map(MAP.to_string());
+//}
+
+//pub fn load_map(map_string: String) {
+   // let wall_texture = load_texture("resources/wall.png").await.expect("Failed to load resource");
+   // let floor_texture = load_texture("resources/floor.png").await.expect("Failed to load resource");
+   // let player_texture = load_texture("resources/player.png").await.expect("Failed to load resource");
+   // let rows: Vec<&str> = map_string.trim().split('\n').map(|x| x.trim()).collect();
+
+   // for (y, &row) in rows.iter().enumerate() {
+   //     let columns: Vec<&str> = row.split(' ').collect();
+
+   //     for (x, &column) in columns.iter().enumerate() {
+   //         let x = x as f32;
+   //         let y = y as f32;
+   //         let position = Position { x, y, z: 0 };
+
+   //        match column {
+   //             "." => create_floor(world, position, floor_texture),
+   //             "W" => create_wall(world, position, wall_texture),
+   //             "P" => {
+   //                 create_floor(world, position, floor_texture);
+   //                 create_player(world, Position { x, y, z: 1 }, player_texture);
+   //             },
+   //             _ => panic!("unrecognized map item")
+   //         } 
+   //     }
+   // }
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
-use arrow_game::{register_components, Game, initialize_level, register_resources};
+use arrow_game::{GameObject, GameManager};
 use macroquad::prelude::*;
-use specs::{World, WorldExt};
-
 
 #[macroquad::main("Arrow Game")]
 async fn main() {
-    let mut world = World::new();
-    register_components(&mut world);
-    register_resources(&mut world);
-    initialize_level(&mut world).await;
-
-    let game = Game::new(world);
+    let mut manager = GameManager::new();
 
     loop {
-        game.update();
-        game.draw();
-
+        manager.input();
+        manager.update();
+        manager.draw();
         next_frame().await;
     } 
 }
