3636e2317a4633ab11e2d2a8d14be44b9c8b1727
alessioarcara <Spammer123@>
2024-03-10 11:05:47 +0100

    Implemented menu with buttons to exit and start the game

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,18 +1,24 @@
+mod menu;
+mod game;
 mod camera;
-mod player;
 
 use bevy::prelude::*;
+use {menu::MenuPlugin, game::GamePlugin, camera::CameraPlugin};
 
-use camera::CameraPlugin;
-use player::PlayerPlugin;
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    Menu,
+    InGame
+}
 
 fn main() {
     App::new()
         // Bevy built-ins
         .insert_resource(ClearColor(Color::rgb(0.1,0.0,0.15)))
-        // User configured plugins.
-        .add_plugins(PlayerPlugin)
-        .add_plugins(CameraPlugin)
         .add_plugins(DefaultPlugins)
+        .init_state::<GameState>()
+        // User configured plugins.
+        .add_plugins((CameraPlugin, MenuPlugin, GamePlugin))
         .run();
 }
--- client/src/state.rs
+++
@@ -1,9 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
-pub enum GameState {
-    Menu,
-    Game
-}
-
-pub struct StatePlugin;
